32c89cd28c15c1dc4214817571820a582fbd8df5
Nat Zimmermann <nat@natzim.me>
2015-06-20 22:25:12 +0100

    Updated welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,12 +18,15 @@
     <div class="section no-pad-bot">
         <div class="container">
             <br><br>
-            <h1 class="header center orange-text">freedr</h1>
+            <div class="center">
+                <img src="/img/logo.png" alt="freedr" width="250" height="250">
+            </div>
+            <h1 class="header center red-text">freedr</h1>
             <div class="row center">
                 <h5 class="header col s12 light">Like Tinder, for freelancers</h5>
             </div>
             <div class="row center">
-                <a href="{{ route('auth.register') }}" class="btn-large waves-effect waves-light orange">Get Started</a>
+                <a href="{{ route('auth.register') }}" class="btn-large waves-effect waves-light red">Get Started</a>
             </div>
             <br><br>
 
@@ -38,7 +41,7 @@
             <div class="row">
                 <div class="col s12 m4">
                     <div class="icon-block">
-                        <h2 class="center light-blue-text"><i class="mdi-editor-attach-money"></i></h2>
+                        <h2 class="center red-text"><i class="mdi-editor-attach-money"></i></h2>
                         <h5 class="center">Free</h5>
 
                         <p class="light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus eius, exercitationem harum hic ipsa ipsum iste quia sunt tenetur voluptates.</p>
@@ -47,7 +50,7 @@
 
                 <div class="col s12 m4">
                     <div class="icon-block">
-                        <h2 class="center light-blue-text"><i class="mdi-navigation-check"></i></h2>
+                        <h2 class="center red-text"><i class="mdi-navigation-check"></i></h2>
                         <h5 class="center">Easy</h5>
 
                         <p class="light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid asperiores beatae consectetur corporis dolores eum, inventore omnis quaerat veritatis vitae?</p>
@@ -56,7 +59,7 @@
 
                 <div class="col s12 m4">
                     <div class="icon-block">
-                        <h2 class="center light-blue-text"><i class="mdi-hardware-phonelink"></i></h2>
+                        <h2 class="center red-text"><i class="mdi-hardware-phonelink"></i></h2>
                         <h5 class="center">Accessible</h5>
 
                         <p class="light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium eligendi eos et ex libero necessitatibus, nulla perspiciatis repellat velit!</p>
